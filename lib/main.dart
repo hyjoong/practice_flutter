7037ecbb07556a58a44668a43b52690b3c9e76c7
hyjoong <maplesosser@naver.com>
2022-07-19 22:35:27 +0900

    feat: slide image 구현 (like swiper & slick) - PageView widget - Timer - PageController

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world/screen/main_screen.dart';
+import 'package:hello_world/screen/preview_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MainScreen(),
+      home: PreviewScreen(),
     ),
   );
 }
 
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.orange,
-//       body: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           Image.asset(
-//             'asset/img/hamster.png',
-//             ),
-//           CircularProgressIndicator(
-//             color: Colors.white,
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
 
---
+++ lib/screen/preview_screen.dart
@@ -0,0 +1,61 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class PreviewScreen extends StatefulWidget {
+  const PreviewScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PreviewScreen> createState() => _PreviewScreenState();
+}
+
+class _PreviewScreenState extends State<PreviewScreen> {
+  Timer? timer;
+  PageController controller = PageController(
+    initialPage: 0,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 3), (timer) {
+      int currentPage = controller.page!.toInt();
+      int nextPage = currentPage + 1;
+
+      if (nextPage > 4) {
+        nextPage = 0;
+      }
+
+      controller.animateToPage(
+        nextPage,
+        duration: Duration(milliseconds: 400),
+        curve: Curves.linear,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    if (timer != null) {
+      timer!.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
+
+    return Scaffold(
+      body: PageView(
+        controller: controller,
+        children: [1, 2, 3, 4, 5]
+            .map((e) => Image.asset('asset/img/ham$e.jpg', fit: BoxFit.cover))
+            .toList(),
+      ),
+    );
+  }
+}
