c52fed08219836194a643831b48d8754743c8319
hyjoong <maplesosser@naver.com>
2022-07-23 22:33:19 +0900

    feat: 난수 생성

--- lib/screen/number_screen.dart
+++ lib/screen/number_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:hello_world/constants/color.dart';
 
@@ -9,6 +11,8 @@ class NumberScreen extends StatefulWidget {
 }
 
 class _NumberScreenState extends State<NumberScreen> {
+  List<int> randomNumbers = [123, 456, 789];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,72 +24,117 @@ class _NumberScreenState extends State<NumberScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '랜덤숫자 생성기',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.settings,
-                        color: RED_COLOR,
-                      ))
-                ],
+              _Header(),
+              _Body(
+                randomNumbers: randomNumbers,
               ),
-              Expanded(
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      123,
-                      456,
-                      789,
-                    ]
-                        .asMap()
-                        .entries
-                    // asMap().entries로 key, value값 가져올 수 있음
-                    // (0:123), (1:456), (2:789)
-                        .map(
-                          (x) => Padding(
-                            padding:
-                                EdgeInsets.only(bottom: x.key == 2 ? 0 :16.0),
-                            child: Row(
-                              children: x.value
-                                  .toString()
-                                  .split('')
-                                  .map(
-                                    (y) => Image.asset(
-                                      'asset/img/$y.png',
-                                      height: 70.0,
-                                      width: 50.0,
-                                    ),
-                                  )
-                                  .toList(),
-                            ),
-                          ),
-                        )
-                        .toList()),
-              ),
-              SizedBox(
-                // Cointainer도 사용되지만 여러 용도로 사용이 가능하지만 width, height만 조절할거라서 SizedBox 사용
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: RED_COLOR, // 활성화 됐을 때 색상
-                  ),
-                  onPressed: () {},
-                  child: Text('생성하기'),
-                ),
-              )
+              _Footer(onPressed: onRandomNumberGenerate),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void onRandomNumberGenerate() {
+    final rand = Random();
+    final Set<int> newNumbers = {}; // 중복 번호가 없게하기 위해서 List 대신 Set
+
+    // for(int i=0;i<3;i++){  // 증복 되는 숫자가 나오면 add하지 않아서 2 or 1개만 들어갈 확률이 있음
+    while (newNumbers.length != 3) {
+      final number = rand.nextInt(1000);
+      newNumbers.add(number);
+    }
+
+    setState(() {
+      randomNumbers = newNumbers.toList();
+    });
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.settings,
+              color: RED_COLOR,
+            ))
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNumbers;
+
+  const _Body({required this.randomNumbers, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: randomNumbers
+              .asMap()
+              .entries
+          // asMap().entries로 key, value값 가져올 수 있음
+          // (0:123), (1:456), (2:789)
+              .map(
+                (x) =>
+                Padding(
+                  padding: EdgeInsets.only(bottom: x.key == 2 ? 0 : 16.0),
+                  child: Row(
+                    children: x.value
+                        .toString()
+                        .split('')
+                        .map(
+                          (y) =>
+                          Image.asset(
+                            'asset/img/$y.png',
+                            height: 70.0,
+                            width: 50.0,
+                          ),
+                    )
+                        .toList(),
+                  ),
+                ),
+          )
+              .toList()),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _Footer({required this.onPressed, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      // Cointainer도 사용되지만 여러 용도로 사용이 가능하지만 width, height만 조절할거라서 SizedBox 사용
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: RED_COLOR, // 활성화 됐을 때 색상
+        ),
+        onPressed: onPressed,
+        child: Text('생성하기'),
       ),
     );
   }
