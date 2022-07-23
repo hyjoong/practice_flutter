6d46e08225bc42537bee6db174b80e1b340b20f3
hyjoong <maplesosser@naver.com>
2022-07-23 17:24:01 +0900

    feat: map 함수 적용

--- lib/screen/number_screen.dart
+++ lib/screen/number_screen.dart
@@ -15,7 +15,8 @@ class _NumberScreenState extends State<NumberScreen> {
       backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16.0),  // fromLTRB(left, top, right, bottom) 으로도 가능
+          padding: EdgeInsets.all(16.0),
+          // fromLTRB(left, top, right, bottom) 으로도 가능
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -40,24 +41,48 @@ class _NumberScreenState extends State<NumberScreen> {
               ),
               Expanded(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('123'),
-                    Text('456'),
-                    Text('789'),
-                  ],
-                ),
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      123,
+                      456,
+                      789,
+                    ]
+                        .asMap()
+                        .entries
+                    // asMap().entries로 key, value값 가져올 수 있음
+                    // (0:123), (1:456), (2:789)
+                        .map(
+                          (x) => Padding(
+                            padding:
+                                EdgeInsets.only(bottom: x.key == 2 ? 0 :16.0),
+                            child: Row(
+                              children: x.value
+                                  .toString()
+                                  .split('')
+                                  .map(
+                                    (y) => Image.asset(
+                                      'asset/img/$y.png',
+                                      height: 70.0,
+                                      width: 50.0,
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ),
+                        )
+                        .toList()),
               ),
               SizedBox(
-                  // Cointainer도 사용되지만 여러 용도로 사용이 가능하지만 width, height만 조절할거라서 SizedBox 사용
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: RED_COLOR, // 활성화 됐을 때 색상
-                    ),
-                    onPressed: () {},
-                    child: Text('생성하기'),
-                  ))
+                // Cointainer도 사용되지만 여러 용도로 사용이 가능하지만 width, height만 조절할거라서 SizedBox 사용
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: RED_COLOR, // 활성화 됐을 때 색상
+                  ),
+                  onPressed: () {},
+                  child: Text('생성하기'),
+                ),
+              )
             ],
           ),
         ),
